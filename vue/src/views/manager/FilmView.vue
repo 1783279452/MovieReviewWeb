56fb94de4ea056fac6182362a0c99cd349e76409
qiwen <2424367803@qq.com>
2024-09-25 09:16:07 +0800

    vue

--- vue/src/views/manager/FilmView.vue
+++ vue/src/views/manager/FilmView.vue
@@ -65,7 +65,7 @@ const loadFilmByCategory = (categoryId) => {
 }
 
 const load = () => {
-  request.get('/film/selectPage', {
+  request.get('/movie/selectPage', {
     params: {
       pageNum: data.pageNum,
       pageSize: data.pageSize,
